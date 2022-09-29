0c1ca66767926ec9d7355d3fea52d803678566d9
Silas Kenneth <silaskenneth1@gmail.com>
2022-09-29 18:17:17 +0300

    Update the code and tests

--- tests/RequestInformationTest.php
+++ tests/RequestInformationTest.php
@@ -1,10 +1,14 @@
 <?php
 
 namespace Microsoft\Kiota\Abstractions\Tests;
+use DateTime;
+use DateTimeZone;
+use InvalidArgumentException;
+use League\Uri\Contracts\UriException;
+use Microsoft\Kiota\Abstractions\HttpMethod;
 use Microsoft\Kiota\Abstractions\RequestInformation;
 use PHPUnit\Framework\TestCase;
 use Microsoft\Kiota\Abstractions\QueryParameter;
-use Rize\UriTemplate;
 
 class RequestInformationTest extends TestCase {
     private RequestInformation $requestInformation;
@@ -13,18 +17,24 @@ class RequestInformationTest extends TestCase {
         $this->requestInformation = new RequestInformation();
     }
 
+    /**
+     * @throws UriException
+     */
     public function testSetUri(): void{
         $pathParameters = [
-            'baseUrl' => 'https://google.com',
+            'baseurl' => 'https://google.com',
             'user%2Did' => 'silas',
         ];
         $queryParameters = ['%24select' => ['subject', 'importance']];
-        $this->requestInformation->urlTemplate = '{+baseUrl}/{user%2Did}/mails{?%24select}';
-        $this->requestInformation->pathParameters = $pathParameters;
+        $this->requestInformation->urlTemplate = '{+baseurl}/{user%2Did}/mails{?%24select}';
+        $this->requestInformation->setPathParameters($pathParameters);
         $this->requestInformation->queryParameters = $queryParameters;
         $this->assertEquals("https://google.com/silas/mails?%24select=subject,importance", $this->requestInformation->getUri());
     }
 
+    /**
+     * @throws UriException
+     */
     public function testSetQueryParameters(): void {
         $this->requestInformation->urlTemplate = '{?%24select,top}';
 
@@ -39,6 +49,48 @@ class RequestInformationTest extends TestCase {
         $this->assertEquals(['displayName', 'age'], $this->requestInformation->queryParameters['%24select']);
         $this->assertArrayHasKey('top', $this->requestInformation->queryParameters);
         $this->assertEquals('?%24select=displayName,age&top=10', $this->requestInformation->getUri());
+    }
+
+    /**
+     * @throws UriException
+     */
+    public function testWillThrowExceptionWhenNoBaseUrl(): void {
+        $this->expectException(InvalidArgumentException::class);
+        $pathParameters = [
+            'bad' => 'https://google.com',
+            'user%2Did' => 'silas',
+        ];
+        $this->requestInformation->urlTemplate = '{+baseurl}/{user%2Did}/mails{?%24select}';
+        $this->requestInformation->setPathParameters($pathParameters);
+        $uri = $this->requestInformation->getUri();
+        $this->assertEquals('', $uri);
+    }
+
+    /**
+     * @throws UriException
+     */
+    public function testPathParametersOfDateTimeOffsetType(): void
+    {
+        // Arrange as the request builders would
+        $requestInfo = new RequestInformation();
+        $requestInfo->httpMethod = HttpMethod::GET;
+        $requestInfo->urlTemplate = "https://localhost/getDirectRoutingCalls(fromDateTime='{fromDateTime}',toDateTime='{toDateTime}')";
+
+        // Act
+        $fromDateTime  =new DateTime();
+        $fromDateTime->setDate(2022, 8, 1);
+        $fromDateTime->setTime(0,0);
+        $fromDateTime->setTimezone(new DateTimeZone('+00:00'));
+        $toDateTime  =new DateTime();
+        $toDateTime->setDate(2022, 8, 2);
+        $toDateTime->setTime(0,0);
+        $toDateTime->setTimezone(new DateTimeZone('+00:00'));
+        $requestInfo->pathParameters["fromDateTime"] = $fromDateTime;
+        $requestInfo->pathParameters["toDateTime"] =  $toDateTime;
+
+        // Assert
+        $uri = $requestInfo->getUri();
+        $this->assertEquals("https://localhost/getDirectRoutingCalls(fromDateTime='2022-08-01T00%3A00%3A00%2B00%3A00',toDateTime='2022-08-02T00%3A00%3A00%2B00%3A00')", $uri);
     }
 }
 
