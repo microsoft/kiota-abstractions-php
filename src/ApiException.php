0c61167499dd4c50883cf1eff5785fb819b9aa38
Philip Gichuhi <pgichuhi@microsoft.com>
2022-10-18 12:53:46 +0300

    Add raw response to API exception

--- src/ApiException.php
+++ src/ApiException.php
@@ -2,9 +2,17 @@
 
 namespace Microsoft\Kiota\Abstractions;
 use Exception;
+use Psr\Http\Message\ResponseInterface;
 
 class ApiException extends Exception
 {
+    /**
+     * Raw response object
+     *
+     * @var ResponseInterface|null
+     */
+    private ?ResponseInterface $response = null;
+
     /**
      * @param string $message
      * @param int $code
@@ -12,5 +20,26 @@ class ApiException extends Exception
      */
     public function __construct(string $message = "", int $code = 0, ?Exception $innerException = null) {
         parent::__construct($message, $code, $innerException);
+    }
+
+    /**
+     * Set raw response from API
+     *
+     * @param ResponseInterface $response
+     * @return void
+     */
+    public function setResponse(ResponseInterface $response): void
+    {
+        $this->response = $response;
+    }
+
+    /**
+     * Return raw response from API
+     *
+     * @return ResponseInterface|null
+     */
+    public function getResponse(): ?ResponseInterface
+    {
+        return $this->response;
     }
 }
