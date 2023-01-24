444851303d63737c38b1a0c2bbe242b0b581f299
Philip Gichuhi <pgichuhi@microsoft.com>
2023-01-24 09:38:06 +0200

    Add error mappings to response handler interface

--- src/ResponseHandler.php
+++ src/ResponseHandler.php
@@ -8,7 +8,8 @@ interface ResponseHandler {
     /**
      * Callback method that is invoked when a response is received.
      * @param ResponseInterface $response The native response object.
+     * @param array<string, array{string, string}>|null $errorMappings map of error status codes to  exception models to deserialize to
      * @return Promise A Promise that represents the asynchronous operation and contains the deserialized response.
      */
-    public function handleResponseAsync(ResponseInterface $response): Promise;
+    public function handleResponseAsync(ResponseInterface $response, ?array $errorMappings = null): Promise;
 }
