59e3bec4190654383560e8ba7eb4a54207405d1a
silaskenneth <silaskenneth1@gmail.com>
2024-04-30 15:44:40 +0300

    Rename the method `getDateIntervalFromString` to `parseDateIntervalFromString`

--- src/Serialization/ParseNodeFromStringTrait.php
+++ src/Serialization/ParseNodeFromStringTrait.php
@@ -14,7 +14,7 @@ trait ParseNodeFromStringTrait
     /**
      * @throws Exception
      */
-    public function getDateIntervalFromString(string $value): DateInterval
+    public function parseDateIntervalFromString(string $value): DateInterval
     {
         $negativeValPosition = strpos($value, '-');
         $invert = 0;
