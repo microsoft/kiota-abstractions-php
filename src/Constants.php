2a65ecbe30046648608ba72c539b87c9f9e27554
Philip Gichuhi <pgichuhi@microsoft.com>
2025-01-28 19:24:16 +0300

    update manifest version

--- src/Constants.php
+++ src/Constants.php
@@ -5,6 +5,6 @@ namespace Microsoft\Kiota\Abstractions;
 final class Constants
 {
     // x-release-please-start-version
-    public const VERSION = '1.4.1';
+    public const VERSION = '1.4.0';
     // x-release-please-end
 }
