4b22ce63c666340e8db3d9cea23248de78455c12
Dan Clarke <6248551+DanClarke-io@users.noreply.github.com>
2025-10-08 15:10:14 +0100

    Enhance ParseNodeFromStringTrait to handle fractional seconds in serialization (#58)
    * Strip fractional seconds from date interval strings
    
    Added regex to strip fractional seconds from interval strings.
    
    * test: enhance date interval parsing tests to strip fractional seconds
    
    * chore: add validation for empty DateInterval strings in parseDateIntervalFromString
    
    * fix: update parseDateIntervalFromString to handle null and empty strings, returning a zero interval
    test: add unit test for empty string returning zero DateInterval
    
    * Apply suggestions from code review
    
    Co-authored-by: Vincent Biret <vincentbiret@hotmail.com>
    
    * Update packages/abstractions/src/Serialization/ParseNodeFromStringTrait.php
    
    Co-authored-by: Vincent Biret <vincentbiret@hotmail.com>
    
    * refactor: remove redundant test for empty string returning zero interval
    
    ---------
    
    Co-authored-by: Vincent Biret <vincentbiret@hotmail.com>

--- src/Serialization/ParseNodeFromStringTrait.php
+++ src/Serialization/ParseNodeFromStringTrait.php
@@ -19,13 +19,23 @@ trait ParseNodeFromStringTrait
         $negativeValPosition = strpos($value, '-');
         $invert = 0;
         $str = $value;
+
         if ($negativeValPosition !== false && $negativeValPosition === 0) {
             // Invert the interval
             $invert = 1;
             $str = substr($value, 1);
         }
+
+        // Strip fractional seconds, e.g., PT33.48S => PT33S
+        $str = preg_replace('/(\d+)\.\d+S$/', '$1S', $str);
+        
+        if (is_null($str)) {
+            throw new Exception("Invalid DateInterval string: '$value'");
+        }
+
         $dateInterval = new DateInterval($str);
         $dateInterval->invert = $invert;
+
         return $dateInterval;
     }
 }
